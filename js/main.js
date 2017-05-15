51ad2ef98080dcdcfcfe946dab59c1c6d00cf6d3
Valentina Miranda <valentina.mirandaj@gmail.com>
2017-05-15 13:59:36 -0400

    llamar id's al js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 function validateForm(){
 	/* Escribe tú código aquí */
+	var nombre = document.getElementById("name").value;
+	var apellido = document.getElementById("lastname").value;
+	var correo = document.getElementById("input-email").value;
+	var pass = document.getElementById("input-password").value;
+	var seleccion = document.getElementsByClassName("form-control").value;
+	var twitter = document.getElementById("input-social");
+
+	if(nombre == 0 || apellido == 0 || correo == 0 || pass == 0 || seleccion == 0 || twitter == 0){
+		var errorNombre = document.createElement("span");
+		errorNombre.innerHTML = "Escribe tu " + ;
+		document.getElementById("").appendChild(errorNombre);
+		return false;
+	}
 }
