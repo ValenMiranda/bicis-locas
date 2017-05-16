bb0b6d5f1d57a0d306b1c1321bb74fd41ff604a1
Valentina Miranda <valentina.mirandaj@gmail.com>
2017-05-15 22:49:53 -0400

    avances en javascript

--- js/main.js
+++ js/main.js
@@ -5,12 +5,41 @@ function validateForm(){
 	var correo = document.getElementById("input-email").value;
 	var pass = document.getElementById("input-password").value;
 	var seleccion = document.getElementsByClassName("form-control").value;
-	var twitter = document.getElementById("input-social");
+	var twitter = document.getElementById("input-social").value;
 
-	if(nombre == 0 || apellido == 0 || correo == 0 || pass == 0 || seleccion == 0 || twitter == 0){
-		var errorNombre = document.createElement("span");
-		errorNombre.innerHTML = "Escribe tu " + ;
-		document.getElementById("").appendChild(errorNombre);
-		return false;
+	if(nombre == ""){
+		var nombreVacio = document.getElementsByClassName('name-container input-box')[0];
+        var span = document.createElement('span');
+        var requiredName = document.createTextNode("Debe ingresar su nombre");
+        nombreVacio.appendChild(span);
+        span.appendChild(requiredName);
+	}
+
+	if(apellido == ""){
+		var apellidoVacio = document.getElementsByClassName('lastname-container input-box')[0];
+        var span = document.createElement('span');
+        var requiredLast = document.createTextNode("Debe ingresar su apellido");
+        apellidoVacio.appendChild(span);
+        span.appendChild(requiredLast);
+        var expReg = /^[a-zA-Z ]+$/;
+		}else(!expReg.test(apellido)){
+			return true;	
+		}
+	}
+
+	if(correo == ""){
+		var correoVacio = document.getElementsByClassName('email-container input-box')[0];
+        var span = document.createElement('span');
+        var requiredCorreo = document.createTextNode("Debe ingresar su correo");
+        correoVacio.appendChild(span);
+        span.appendChild(requiredCorreo);
+	}
+
+	if(pass == ""){
+		var passVacio = document.getElementsByClassName('form-group input-box')[0];
+        var span = document.createElement('span');
+        var requiredPass = document.createTextNode("Debe ingresar una contraseña");
+        passVacio.appendChild(span);
+        span.appendChild(requiredPass);
 	}
 }
